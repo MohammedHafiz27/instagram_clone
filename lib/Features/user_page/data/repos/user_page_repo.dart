cdd50008cb4d3b1fc1ec0ff0deed2affc7983bf2
mohammed hafiz <mohammed.hafiz.2710@gmail.com>
2025-09-02 18:39:21 +0300

    PostsReelsModel repo done

--- lib/Features/user_page/data/repos/user_page_repo.dart
+++ lib/Features/user_page/data/repos/user_page_repo.dart
@@ -1,12 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:instagram_clone/Core/errors/failures.dart';
 import 'package:instagram_clone/Features/user_page/data/models/followers_model/followers_model.dart';
+import 'package:instagram_clone/Features/user_page/data/models/posts_reels_model/posts_reels_model.dart';
 
 abstract class UserPageRepo {
-  Future<Either<Failure, FollowersModel>> getFollowers({
-    required String userId,
-  });
-  Future<Either<Failure, FollowersModel>> getFollowing({
-    required String userId,
-  });
+  Future<Either<Failure, FollowersModel>> getFollowers({required String userId});
+  Future<Either<Failure, FollowersModel>> getFollowing({required String userId});
+  Future<Either<Failure, PostsReelsModel>> getPostsAndReels({required String userId});
 }
--- lib/Features/user_page/data/repos/user_page_repo_imple.dart
+++ lib/Features/user_page/data/repos/user_page_repo_imple.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:instagram_clone/Core/errors/failures.dart';
 import 'package:instagram_clone/Core/utils/api_service.dart';
 import 'package:instagram_clone/Features/user_page/data/models/followers_model/followers_model.dart';
+import 'package:instagram_clone/Features/user_page/data/models/posts_reels_model/posts_reels_model.dart';
 import 'package:instagram_clone/Features/user_page/data/repos/user_page_repo.dart';
 
 class UserPageRepoImple implements UserPageRepo {
@@ -10,13 +11,9 @@ class UserPageRepoImple implements UserPageRepo {
 
   UserPageRepoImple(this.apiService);
   @override
-  Future<Either<Failure, FollowersModel>> getFollowers({
-    required String userId,
-  }) async {
+  Future<Either<Failure, FollowersModel>> getFollowers({required String userId}) async {
     try {
-      var response = await apiService.getData(
-        "/v1/followers?username_or_id_or_url=$userId&amount=100",
-      );
+      var response = await apiService.getData("/v1/followers?username_or_id_or_url=$userId&amount=100");
       return Right(FollowersModel.fromJson(response));
     } catch (e) {
       if (e is DioException) {
@@ -27,14 +24,23 @@ class UserPageRepoImple implements UserPageRepo {
   }
 
   @override
-  Future<Either<Failure, FollowersModel>> getFollowing({
-    required String userId,
-  }) async {
+  Future<Either<Failure, FollowersModel>> getFollowing({required String userId}) async {
     try {
-      var response = await apiService.getData(
-        "/v1/following?username_or_id_or_url=$userId&amount=100",
-      );
+      var response = await apiService.getData("/v1/following?username_or_id_or_url=$userId&amount=100");
       return Right(FollowersModel.fromJson(response));
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, PostsReelsModel>> getPostsAndReels({required String userId}) async {
+    try {
+      var response = await apiService.getData("/v1/posts?username_or_id_or_url=$userId");
+      return Right(PostsReelsModel.fromJson(response));
     } catch (e) {
       if (e is DioException) {
         return Left(ServerFailure.fromDioError(e));
