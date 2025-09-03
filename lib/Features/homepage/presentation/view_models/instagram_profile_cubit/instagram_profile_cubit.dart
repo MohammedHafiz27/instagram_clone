22100e793c62e510b4cea3b701f0b87c47e943ef
mohammed hafiz <mohammed.hafiz.2710@gmail.com>
2025-09-03 12:17:24 +0300

    use Future.wait for better api calls

--- lib/Features/homepage/presentation/view_models/instagram_profile_cubit/instagram_profile_cubit.dart
+++ lib/Features/homepage/presentation/view_models/instagram_profile_cubit/instagram_profile_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:instagram_clone/Core/errors/failures.dart';
 import 'package:instagram_clone/Features/homepage/data/models/instagram_profile_model/instagram_profile_model.dart';
 
 import 'package:instagram_clone/Features/homepage/data/repos/homepage_repo.dart';
@@ -10,46 +12,39 @@ import 'package:instagram_clone/Features/user_page/data/repos/user_page_repo.dar
 part 'instagram_profile_state.dart';
 
 class InstagramProfileCubit extends Cubit<InstagramProfileState> {
-  InstagramProfileCubit(this.homepageRepo, this.userPageRepo)
-    : super(InstagramProfileInitial());
+  InstagramProfileCubit(this.homepageRepo, this.userPageRepo) : super(InstagramProfileInitial());
   final HomepageRepo homepageRepo;
   final UserPageRepo userPageRepo;
 
   Future<void> loadUserData(String username) async {
     emit(InstagramProfileLoading());
 
-    final profileResult = await homepageRepo.getInstagramProfile(username);
-    final followersResult = await userPageRepo.getFollowers(userId: username);
-    final followingResult = await userPageRepo.getFollowing(userId: username);
-    final postsAndReelsResult = await userPageRepo.getPostsAndReels(userId: username);
+    final results = await Future.wait([
+      homepageRepo.getInstagramProfile(username),
+      userPageRepo.getFollowers(userId: username),
+      userPageRepo.getFollowing(userId: username),
+      userPageRepo.getPostsAndReels(userId: username),
+    ]);
+    final profileResult = results[0] as Either<Failure, InstagramProfileModel>;
+    final followersResult = results[1] as Either<Failure, FollowersModel>;
+    final followingResult = results[2] as Either<Failure, FollowersModel>;
+    final postsAndReelsResult = results[3] as Either<Failure, PostsReelsModel>;
 
-    profileResult.fold(
-      (failure) => emit(InstagramProfileFailure(failure.errorMessage)),
-      (profile) {
-        followersResult.fold(
-          (failure) => emit(InstagramProfileFailure(failure.errorMessage)),
-          (followers) {
-            followingResult.fold(
-              (failure) => emit(InstagramProfileFailure(failure.errorMessage)),
-              (following) {
-                postsAndReelsResult.fold(
-                  (failure) => emit(InstagramProfileFailure(failure.errorMessage)),
-                  (postsAndReels) {
-                    emit(
-                      InstagramProfileSuccess(
-                        profile: profile,
-                        followers: followers,
-                        following: following,
-                        postsAndReels: postsAndReels,
-                      ),
-                    );
-                  },
-                );
-              },
+    profileResult.fold((failure) => emit(InstagramProfileFailure(failure.errorMessage)), (profile) {
+      followersResult.fold((failure) => emit(InstagramProfileFailure(failure.errorMessage)), (followers) {
+        followingResult.fold((failure) => emit(InstagramProfileFailure(failure.errorMessage)), (following) {
+          postsAndReelsResult.fold((failure) => emit(InstagramProfileFailure(failure.errorMessage)), (postsAndReels) {
+            emit(
+              InstagramProfileSuccess(
+                profile: profile,
+                followers: followers,
+                following: following,
+                postsAndReels: postsAndReels,
+              ),
             );
-          },
-        );
-      },
-    );
+          });
+        });
+      });
+    });
   }
 }
